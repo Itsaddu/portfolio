fa2821ff89d66fa63d6f94661a6764fe160b6788
Adwaith J S <92678645+Itsaddu@users.noreply.github.com>
2026-02-18 13:52:06 +0530

    Enhance player.js to support season and episode parameters

--- my-stream-app/public/js/player.js
+++ my-stream-app/public/js/player.js
@@ -1,17 +1,15 @@
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 const type = params.get("type");
-
-if (!id || !type) {
-    window.location.href = "home.html";
-}
+const season = params.get("season");
+const episode = params.get("episode");
 
 let embedURL = "";
 
 if (type === "movie") {
     embedURL = `https://www.vidking.net/embed/movie/${id}`;
 } else {
-    embedURL = `https://www.vidking.net/embed/tv/${id}/1/1`;
+    embedURL = `https://www.vidking.net/embed/tv/${id}/${season || 1}/${episode || 1}`;
 }
 
 document.getElementById("player").src = embedURL;
